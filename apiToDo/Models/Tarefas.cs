a2bcb7ea3bdd20a0bc9308efe6b86c1497ed0edb
Hugo César <hugocesarvs802@outlook.com>
2022-06-24 10:42:32 -0300

    Organização de métodos, tratamento de ID de tarefa ao salvar novo registro.

--- apiToDo/Models/Tarefas.cs
+++ apiToDo/Models/Tarefas.cs
@@ -26,51 +26,6 @@ namespace apiToDo.Models
             }
         };
 
-        public List<TarefaDTO> Listar()
-        {
-            try
-            {
-                return listaTaerfas;
-            }
-            catch(Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public bool Inserir(TarefaDTO tarefa)
-        {
-            try
-            {
-                var lista = Listar();
-                lista.Add(tarefa);
-
-                return true;
-            }
-            catch(Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public bool Deletar(int idTarefa)
-        {
-            try
-            {
-                var lista = Listar();
-                var tarefa = lista.FirstOrDefault(l => l.IdTarefa == idTarefa);
-
-                if (lista.Remove(tarefa))
-                    return true;
-
-                throw new Exception("Falha ao remover registro de tarefa, tente novamente.");
-            }
-            catch(Exception ex)
-            {
-                throw ex;
-            }
-        }
-
         public bool Atualizar(TarefaDTO tarefa)
         {
             try
@@ -97,6 +52,53 @@ namespace apiToDo.Models
             try
             {
                 return Listar().FirstOrDefault(l => l.IdTarefa == idTarefa);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool Deletar(int idTarefa)
+        {
+            try
+            {
+                var lista = Listar();
+                var tarefa = lista.FirstOrDefault(l => l.IdTarefa == idTarefa);
+
+                if (lista.Remove(tarefa))
+                    return true;
+
+                throw new Exception("Falha ao remover registro de tarefa, tente novamente.");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool Inserir(TarefaDTO tarefa)
+        {
+            try
+            {
+                var lista = Listar();
+
+                tarefa.IdTarefa = lista.Last().IdTarefa + 1;
+                lista.Add(tarefa);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<TarefaDTO> Listar()
+        {
+            try
+            {
+                return listaTaerfas;
             }
             catch (Exception ex)
             {
